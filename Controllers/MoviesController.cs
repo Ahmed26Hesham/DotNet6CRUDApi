4690cc459267c4dd1971768e2048cc0153fb4ac4
Ahmed Hesham <ahmedhesham2020s@gmail.com>
2022-10-08 13:03:04 +0200

    V1.0

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,153 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Services;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMoviesService _moviesService;
+        private readonly IGenresService _genresService;
+        private new List<String> _allowedExtensions = new List<string>{".jpg" , ".png"};
+        private long _maxAllowedPosterSize = 1048576;
+
+        public MoviesController(IMoviesService moviesService, IGenresService genresService, IMapper mapper)
+        {
+            this._moviesService = moviesService;
+            _genresService = genresService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            //***************
+            var movies = await _moviesService.GetAll();
+            // Map movies to MovieDetailsDto
+
+            var data = _mapper.Map<IEnumerable<MovieDetailsDto>>(movies);
+
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var movie = await _moviesService.GetById(id);
+            
+            if(movie == null)   
+                return NotFound();
+
+            var dto = _mapper.Map<MovieDetailsDto>(movie);
+
+            return Ok(dto);
+        }
+
+        //*********************************
+
+        [HttpGet("GetByGenreId")]  // لما بعمل كدا ببعتها عن طريق الكويري استرينج
+                                   // مش ف السيجمينت
+        public async Task<IActionResult> GetByGenreIdAsync(byte GenreId)
+        {
+            var movies = await _moviesService.GetAll(GenreId);
+            // Map movies to MovieDetailsDto
+
+            var data = _mapper.Map<IEnumerable<MovieDetailsDto>>(movies);
+
+            return Ok(data);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm] MovieDto dto)  // *******************
+        {
+            if (dto.Poster == null)
+                return BadRequest("Poster is required");
+
+            if (!_allowedExtensions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+            {
+                return BadRequest("Only .png and .jpg are allowed");
+            }
+
+            if(dto.Poster.Length > _maxAllowedPosterSize)
+                return BadRequest("Max Allowed size for poster is 1MB");
+
+            var isValidGenre = await _genresService.IsValidGenre(dto.GenreId);
+
+            if (!isValidGenre)
+                return BadRequest("Invalid Genre Id");
+
+            using var dataStream = new MemoryStream();
+            await dto.Poster.CopyToAsync(dataStream);
+
+            var movie = _mapper.Map<Movie>(dto);
+            movie.Poster = dataStream.ToArray();
+
+            _moviesService.Add(movie);
+
+            return Ok(movie);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id , [FromForm] MovieDto dto)
+        {
+            var movie = await _moviesService.GetById(id);
+
+            if (movie == null)
+                return NotFound($"No Movie was found with ID {id}");
+
+            var isValidGenre = await _genresService.IsValidGenre(dto.GenreId);
+
+            if (!isValidGenre)
+                return BadRequest("Invalid Genre Id");
+
+            if(dto.Poster != null)
+            {
+                if (!_allowedExtensions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+                {
+                    return BadRequest("Only .png and .jpg are allowed");
+                }
+
+                if (dto.Poster.Length > _maxAllowedPosterSize)
+                    return BadRequest("Max Allowed size for poster is 1MB");
+
+                using var dataStream = new MemoryStream();
+                await dto.Poster.CopyToAsync(dataStream);
+
+                movie.Poster = dataStream.ToArray();
+
+            }
+
+            movie.StoryLine = dto.StoryLine;
+            movie.Year = dto.Year;
+            movie.GenreId = dto.GenreId;
+            movie.Title = dto.Title;
+            movie.Rate = dto.Rate;
+
+            _moviesService.Update(movie);
+            return Ok(movie);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var movie = await _moviesService.GetById(id);
+
+            if (movie == null)
+                return NotFound($"No Movie was found with ID {id}");
+
+            _moviesService.Delete(movie);
+
+            return Ok(movie);
+        }
+    }
+}
---
+++ Services/IGenresService.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviesApi.Services
+{
+    public interface IGenresService
+    {
+        Task<IEnumerable<Genre>> GetAll();
+        Task<Genre> GetById(byte id);
+        Task<Genre> Add(Genre Genre);
+        Genre Update(Genre Genre);
+        Genre Delete(Genre Genre);
+        Task<bool> IsValidGenre(byte id);
+    }
+}
